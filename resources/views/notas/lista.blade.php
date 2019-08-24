5d6a73251eda5da14447191fb7840a2394ac598e
JuanJosexDD7 <JuanJosexDD7>
2019-08-24 19:23:58 -0400

    Practica Finalizada

---
+++ resources/views/notas/lista.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>Lista de Notas para {{auth()->user()->name}}</span>
+                    <a href="/notas/create" class="btn btn-primary btn-sm">Nueva Nota</a>
+                </div>
+
+                <div class="card-body">      
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Descripción</th>
+                            <th scope="col">Acción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($notas as $item)
+                            <tr>
+                                <th scope="row">{{ $item->id }}</th>
+                                <td>{{ $item->nombre }}</td>
+                                <td>{{ $item->descripcion }}</td>
+                                <td>
+									<a href="{{ route('notas.editar', $item)}}" class="btn btn-warning btn-sm"><i class="fas fa-pencil-alt"></i></a>
+                                    <form class="d-inline" method="POST" action="{{ route('notas.destroy', $item) }}">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button>
+                                </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{$notas->links()}}
+                {{-- fin card body --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
